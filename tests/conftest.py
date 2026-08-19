981c7495a3fad103221cd15393cb8b31a5860b02
Roman Vohradnik <vohradnik@eldis.cz>
2026-08-19 19:57:52 +0200

    Fix4

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
-"""Test configuration for VS Chrudim."""
-
-from __future__ import annotations
+import pytest
 
 pytest_plugins = "pytest_homeassistant_custom_component"
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+"""Enable loading custom integrations."""
+yield
