f50e78d9ce27c5bff967c6670f03aade0c2d14b3
Roman Vohradnik <vohradnik@eldis.cz>
2026-08-20 07:19:02 +0200

    Fix5

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import pytest
 
 pytest_plugins = "pytest_homeassistant_custom_component"
 
+
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
-"""Enable loading custom integrations."""
-yield
+    """Enable loading custom integrations."""
+    yield
